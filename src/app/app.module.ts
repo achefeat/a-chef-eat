9037a0bc6ec8042acfb1926b97e727096a0e6e20
yerkenov <almukhammedyerkenov@gmail.com>
2019-03-26 12:55:24 +0600

    k

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContentComponent } from './content/content.component';
+import { NavigationbarComponent } from './navigationbar/navigationbar.component';
+import { AboutComponent } from './about/about.component';
+import { SliderComponent } from './slider/slider.component';
+import { ServicesComponent } from './services/services.component';
+import { TeamComponent } from './team/team.component';
+import { GetComponent } from './get/get.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContentComponent
+    ContentComponent,
+    NavigationbarComponent,
+    AboutComponent,
+    SliderComponent,
+    ServicesComponent,
+    TeamComponent,
+    GetComponent
   ],
   imports: [
     BrowserModule,
